56b85a56fdd54cc541f8bdbf48a5e10096b59a39
rr2x <79620740+rr2x@users.noreply.github.com>
2021-12-16 10:54:55 +0800

    current bug fixing: authentication_user_groups

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,4 +1,32 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+
+
+# current problem:
+# OperationalError at /admin/authentication/user/1/change/
+# no such table: authentication_user_groups
+
+# https://docs.djangoproject.com/en/4.0/topics/auth/customizing/
+
+class UserChangeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('email', 'password')
+
+
+class UserCreationForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('email', 'password',)
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
 
 admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
--- authentication/models.py
+++ authentication/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
+
 class UserManager(BaseUserManager):
     def create_user(self, email, password):
 
@@ -37,6 +38,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_superuser = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    # last_login = automatically created by django
 
     USERNAME_FIELD = 'email'
 
