c6fc964e25c8abacc92de9ddbf153a761e84f8f4
rr2x <79620740+rr2x@users.noreply.github.com>
2022-01-03 16:36:37 +0800

    setup static files and initial files for templates

--- votingwebsite/settings.py
+++ votingwebsite/settings.py
@@ -26,6 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# False = activates the default 404 server error
 DEBUG = True
 
 TIME_ZONE = 'Asia/Singapore'
@@ -129,7 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'votingwebsite/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- votingwebsite/urls.py
+++ votingwebsite/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('x/', admin.site.urls),
-    path('', include('authentication.urls')),
+    path('', views.FrontPage, name="frontpage"),
+    path('auth/', include('authentication.urls')),
 ]
