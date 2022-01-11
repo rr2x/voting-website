00adc430e2954dcc7a430f7f5ac38e4e22e42c8c
rr2x <79620740+rr2x@users.noreply.github.com>
2022-01-12 07:32:47 +0800

    setup syle, start design of login form

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
 def welcometoauth(request):
     return render(request, 'authentication/index.html')
+
+
+def login(request):
+
+    if request.method == 'POST':
+        return redirect(reverse('frontpage'))
+
+    return render(request, 'authentication/login.html')
